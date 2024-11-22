594aa1aafaeace3b98467a8343e5a32dbc8d5ee9
a-mancodesss <079bct011.aman@pcampus.edu.np>
2024-11-22 16:25:34 +0545

    logger exception filters

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useLogger(app.get(MyLoggerService))
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
