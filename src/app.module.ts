4fe4591e2eeb30b2950f3b41a6acef261fe4dcb1
a-mancodesss <079bct011.aman@pcampus.edu.np>
2024-11-22 14:44:09 +0545

    added 10 req/min rate lmiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,25 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+    UsersModule,
+     DatabaseModule,
+      EmployeesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    AppService],
 })
 export class AppModule {}
